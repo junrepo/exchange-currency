a0c223b83dacf80083ade0aebe8e7b6ece71108a
Jun Heo <hbuk04@naver.com>
2022-12-19 15:15:00 +0900

    commit

--- main.js
+++ main.js
@@ -6,6 +6,14 @@
     5. 바로 아래 단위가 바뀐다.
     6. 환전 금액이 반대편에 표시된다.
     7. (항상 반대편의 단위는 교환된다)
+
+    2. 환율정보 들고오기
+    3. 드롭다운 리스트에서 아이템 선택하면 아이템이 바뀜
+    ---
+    4. 금액을 입력하면 환전이 된다
+    5. 드롭다운 리스트에서 아이템을 선택하면 다시 그 단위 기준으로 환전이 됨
+    6. 숫자를 한국어로 읽는 법
+    7. 반대로 밑에 박스에서 숫자를 바꿔도 위에 박스에 환율이 적용이 된다!
 */
 
 let currencyRatio = {
@@ -14,19 +22,40 @@ let currencyRatio = {
         KRW: 1310.38,
         USD: 1,
         VND: 23525.00,
-        unit: "usd"
+        unit: "달러"
     },
 
     KRW : {
         KRW: 1,
         USD: 0.00076,
-        VDN: 17.95
+        VDN: 17.95,
+        unit: "원"
     },
 
     VDN: {
         KRW: 0.056,
         USD: 0.000043,
-        VND: 1
+        VND: 1,
+        unit: "동"
     }
 }
+let fromCurrency = 'USD';
+let toCurrency = 'USD';
+
+document.querySelectorAll("#from-currency-list a")
+.forEach(menu=>menu.addEventListener("click", function(){
+    // 1. 버튼을 가져온다
+    // 2. 버튼의 값을 바꾼다
+    document.getElementById("from-button").textContent = this.textContent;
+    // 3. 선택된 currency값을 변수에 저장해준다.
+    fromCurrency = this.textContent;
+    console.log(fromCurrency);
+}));
+
+document.querySelectorAll("#to-currency-list a")
+.forEach(menu=>menu.addEventListener("click", function(){
+    document.getElementById("to-button").textContent = this.textContent;
+    toCurrency = this.textContent;
+    console.log(toCurrency);
+}))
 
